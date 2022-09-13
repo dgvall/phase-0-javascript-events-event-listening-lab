d1dc65b73afd4c19633dfac596d3dfadc5f7d2e8
Diego Valles <dgvall134@gmail.com>
2022-09-13 16:46:48 -0600

    commit tree 0ae6bdc517176b3e97b526d9978009408a482e8e

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+function alertMe () {
+    return alert(`WHY DID YOU CLICK THE BUTTON??`)
+}
+
 function addingEventListener() {
+    const btn = document.getElementById("button")
+    btn.addEventListener(`click`, alertMe)
 }
